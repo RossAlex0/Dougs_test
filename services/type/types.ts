61af58d6bed366109245eb2ef5e900db189d554d
Alex Rossignol <alexrossignol@macbookair.home>
2024-11-07 14:02:41 +0100

    Add interface Ts

--- services/type/types.ts
+++ services/type/types.ts
@@ -25,3 +25,33 @@ export type Stats = {
   outcomesTotal: number;
   balanceTotal: number;
 };
+
+export interface DetailInterface {
+  categoryGrp_id: number;
+  category_label: string;
+  category_color: string;
+  label: string;
+  amount: number;
+  description: string;
+  date: string;
+}
+
+export interface StateDetail {
+  amount: string | number;
+  description: string;
+  categoryId: string | number;
+}
+
+export interface DetailContext {
+  detailsValue: StateDetail | null;
+  setDetailsValue: (state: StateDetail) => void;
+}
+
+export interface StateCategories {
+  id: number;
+  label: string;
+  description: string;
+  groupId: number;
+  groupLabel: string;
+  groupColor: string;
+}
