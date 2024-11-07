53e98b88341723e0990e22ad78f193a84f0ba85d
Alex Rossignol <alexrossignol@macbookair.home>
2024-11-07 04:39:20 +0100

    Updated home Screen - style - data & pull refresh

--- app/(Home)/index.tsx
+++ app/(Home)/index.tsx
@@ -1,12 +1,116 @@
-import { Link } from "expo-router";
-import { SafeAreaView, Text } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import {
+  FlatList,
+  RefreshControl,
+  SafeAreaView,
+  Text,
+  View,
+} from "react-native";
+import Input from "../../components/Input/Input";
+import Item from "../../components/Item/Item";
+import { Operation, Stats } from "../../services/type/types";
+import {
+  getAllOperations,
+  getOperationsStats,
+} from "../../services/request/get";
+import { numberWithSpace } from "../../services/utils/formatNumber";
+import { groupByDate } from "../../services/utils/formatDate";
+
+import { homeStyle } from "./style";
+import { colors } from "../../style/globalsStyle";
 
 export default function Home() {
+  const [inputValue, SetInputValue] = useState<string | undefined>(undefined);
+  const [statsData, setStatsData] = useState<Stats | undefined>();
+  const [operationsData, setOperationsData] = useState<
+    Operation[] | undefined
+  >();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const getHomeData = async () => {
+    const [stats, operations] = await Promise.all([
+      getOperationsStats(),
+      getAllOperations(),
+    ]);
+    setStatsData(stats);
+    setOperationsData(operations);
+  };
+
+  useEffect(() => {
+    getHomeData();
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    getHomeData().finally(() => setRefreshing(false));
+  }, []);
+
+  const operationListByDate = operationsData && groupByDate(operationsData);
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <Text>Home</Text>
-      <Link href={"/Categories"}>Go to cat</Link>
-      <Link href={"/Details"}>Go to det</Link>
-    </SafeAreaView>
+    <View style={homeStyle.container}>
+      <View style={homeStyle.container_header}>
+        <Input
+          tools={{
+            value: inputValue,
+            placeholder: "Rechercher un élément",
+            keyType: "search",
+            keyboardType: "default",
+          }}
+        />
+        <View style={homeStyle.header_stats}>
+          {statsData && (
+            <>
+              <View style={homeStyle.stats_box}>
+                <Text
+                  style={[homeStyle.stats_nb, { color: colors.title_color }]}
+                >
+                  {numberWithSpace(Math.ceil(statsData.incomesTotal))} €
+                </Text>
+                <Text style={homeStyle.stats_label}>Crédit</Text>
+              </View>
+              <View style={homeStyle.stats_box}>
+                <Text style={[homeStyle.stats_nb, { color: "#FD755F" }]}>
+                  {numberWithSpace(Math.ceil(statsData.outcomesTotal))} €
+                </Text>
+                <Text style={homeStyle.stats_label}>Débit</Text>
+              </View>
+              <View style={homeStyle.stats_box}>
+                <Text style={[homeStyle.stats_nb, { color: "#0ED094" }]}>
+                  {numberWithSpace(Math.ceil(statsData.balanceTotal))} €
+                </Text>
+                <Text style={homeStyle.stats_label}>Solde</Text>
+              </View>
+            </>
+          )}
+        </View>
+      </View>
+      {operationListByDate && (
+        <FlatList
+          data={Object.keys(operationListByDate)}
+          keyExtractor={(item) => item}
+          renderItem={({ item: date }) => (
+            <View>
+              <View style={homeStyle.body_date}>
+                <Text>{date}</Text>
+              </View>
+              {operationListByDate[date].map((operation) => (
+                <Item key={operation.id} operation={operation} />
+              ))}
+            </View>
+          )}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor={colors.white_color}
+              colors={[colors.white_color, colors.primary_color]}
+              progressBackgroundColor={colors.primary_color}
+            />
+          }
+          style={homeStyle.container_body}
+        />
+      )}
+    </View>
   );
 }
